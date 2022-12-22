02c71f2e1b833eabe26aa31e80203639e2cdcd8e
Vadym Piatkovskyi <aiozz@hotmail.com>
2022-12-22 19:01:26 +0200

    Added initial project

---
+++ LessCards/LessCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessCardsApp.swift
+//  LessCards
+//
+//  Created by Vadym Piatkovskyi on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct LessCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
